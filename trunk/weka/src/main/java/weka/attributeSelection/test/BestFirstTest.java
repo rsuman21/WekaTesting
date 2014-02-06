5a7d80eaeae5b86429edcc7f4d945d009ddf657a
mdost <mariam.dost09@gmail.com>
2014-02-06 10:59:36 -0700

    Best first stuff:: Yellllooooo!!!!!!!!!!!

---
+++ trunk/weka/src/main/java/weka/attributeSelection/test/BestFirstTest.java
@@ -0,0 +1,111 @@
+package weka.attributeSelection.test;
+
+import junit.framework.TestCase;
+
+public class BestFirstTest extends TestCase {
+
+	public BestFirstTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testGetRevision() {
+		fail("Not yet implemented");
+	}
+
+	public void testSearch() {
+		fail("Not yet implemented");
+	}
+
+	public void testGlobalInfo() {
+		fail("Not yet implemented");
+	}
+
+	public void testBestFirst() {
+		fail("Not yet implemented");
+	}
+
+	public void testListOptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetOptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetLookupCacheSize() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetLookupCacheSize() {
+		fail("Not yet implemented");
+	}
+
+	public void testLookupCacheSizeTipText() {
+		fail("Not yet implemented");
+	}
+
+	public void testStartSetTipText() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetStartSet() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetStartSet() {
+		fail("Not yet implemented");
+	}
+
+	public void testSearchTerminationTipText() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetSearchTermination() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetSearchTermination() {
+		fail("Not yet implemented");
+	}
+
+	public void testDirectionTipText() {
+		fail("Not yet implemented");
+	}
+
+	public void testSetDirection() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetDirection() {
+		fail("Not yet implemented");
+	}
+
+	public void testGetOptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	public void testPrintGroup() {
+		fail("Not yet implemented");
+	}
+
+	public void testResetOptions() {
+		fail("Not yet implemented");
+	}
+
+	public void testAttributeList() {
+		fail("Not yet implemented");
+	}
+
+}
