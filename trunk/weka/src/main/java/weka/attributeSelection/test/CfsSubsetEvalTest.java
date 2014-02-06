04d300113172ce47bc325934d302266dacc33c59
ShaddaMohammed <shada92@yahoo.com>
2014-02-06 10:18:44 -0700

    Commit three tests

---
+++ trunk/weka/src/main/java/weka/attributeSelection/test/CfsSubsetEvalTest.java
@@ -0,0 +1,19 @@
+package weka.attributeSelection.test;
+
+import junit.framework.TestCase;
+
+public class CfsSubsetEvalTest extends TestCase {
+
+	public CfsSubsetEvalTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
---
+++ trunk/weka/src/main/java/weka/attributeSelection/test/CorrelationAttributeEvalTest.java
@@ -0,0 +1,19 @@
+package weka.attributeSelection.test;
+
+import junit.framework.TestCase;
+
+public class CorrelationAttributeEvalTest extends TestCase {
+
+	public CorrelationAttributeEvalTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
